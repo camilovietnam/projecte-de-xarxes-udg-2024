5b61491898380586f3a9e5dfa0e691ca3bc502ee
Uran Camilo <camilovietnam@gmail.com>
2024-05-03 00:13:09 +0200

    afegir variable SERVER_NAME

--- projecte-de-xarxes/handler.go
+++ projecte-de-xarxes/handler.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 func Handle(w http.ResponseWriter, r *http.Request) {
-	var input []byte
+	var (
+		input      []byte
+		serverName = os.Getenv("SERVER_NAME")
+	)
 
 	if r.Body != nil {
 		defer r.Body.Close()
@@ -18,5 +22,5 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Body: %s", string(input))))
+	w.Write([]byte(fmt.Sprintf("[%s] %s", serverName, string(input))))
 }
